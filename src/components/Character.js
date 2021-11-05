64e468444ca38c56983e155567111bfc096b886b
SheebaHashmi <sheebahashmi0@gmail.com>
2021-11-05 14:14:32 -0400

    Render characters components

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,32 @@
 // Write your Character component here
+import React from "react";
+import styled from 'styled-components';
+
+const Accordian = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    border: 2px solid green;
+    width: 50%;
+    margin: 0 25%;
+    
+    h2{
+        padding: 0 2%;
+    }
+    div{
+        padding-right: 2%;
+    }
+
+`;
+
+function Character({ character }){
+    return (
+        <Accordian>
+        <h2>{character.name}</h2>
+        <div>{character.birth_year}</div>
+
+        </Accordian>
+    )
+}
+export default Character;
